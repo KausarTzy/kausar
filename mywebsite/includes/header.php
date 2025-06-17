4dccaea84af02e64faa4f2a63ed94e1191dc88cc
KausarTzy <kausarandhika79@gmail.com>
2025-06-17 07:21:26 +0700

    Add files via upload

---
+++ mywebsite/includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>My Website</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">My Website</a>
+        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php">Beranda</a></li>
+                <li class="nav-item"><a class="nav-link" href="tentang.php">Tentang Saya</a></li>
+                <li class="nav-item"><a class="nav-link" href="keahlian.php">Keahlian</a></li>
+                <li class="nav-item"><a class="nav-link" href="kontak.php">Kontak</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container mt-4">
